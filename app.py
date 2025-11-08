9673bb6b989f9129dd57dda2b5be01441bd0a956
wal <profes2026@outlook.com>
2025-11-08 00:43:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template 
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  
+    return render_template('index.html')  
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html') 
+    return render_template('sobre.html') 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )

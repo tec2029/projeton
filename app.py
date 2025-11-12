4519ca6cd6a381eb9864c1eee1c8989601ea1cc9
wal <profes2026@outlook.com>
2025-11-12 18:10:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-from flask import Flask, render_template 
+from flask import Flask
 
 app = Flask(__name__) 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  
+    return 'renderizando'
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html') 
+    return 'seguindo..'
 
 if __name__ == '__main__':
     app.run(
